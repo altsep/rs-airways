b144a7831506346c89a3ec064a0818f83ee468b0
Stanlys <3995782+stanlys@users.noreply.github.com>
2023-05-04 20:36:25 +0300

    feat: add enviroment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, StoreModule.forRoot({}, {}), CoreModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, StoreModule.forRoot({}, {}), CoreModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth())],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent],
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'rss-airline',
+    appId: '1:934119809856:web:51cb7b4710369860e0d0a5',
+    storageBucket: 'rss-airline.appspot.com',
+    apiKey: 'AIzaSyA_mLi5QCZCmPxkauNsw5JOwcEeGium7Ds',
+    authDomain: 'rss-airline.firebaseapp.com',
+    messagingSenderId: '934119809856',
+    measurementId: 'G-NMECC43RG0',
+  },
+};
